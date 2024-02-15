8cefcbecd1521afb64292802ae0ffc677cf28e91
ashish dubey <ashishbusiness743@gmail.com>
2024-02-15 14:20:53 +0530

    create UI for login

---
+++ public/pages/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../assets/styles.css">
+</head>
+<body>
+    <div class="container">
+        <div class="heading">
+            <span>Sign In</span>
+        </div>
+        <div class="username">
+            <input type="text" placeholder="Enter Username">
+        </div>
+        <div class="password">
+            <input type="text" placeholder="Enter Password">
+        </div>
+        <div class="login">
+            <button type="button"> Log In</button>
+        </div>
+        
+    </div>
+</body>
+</html>
